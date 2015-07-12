851f288c15f3cda45a8d752003a9acd9bc3e5f4b
Proxy13 <mproxy1453@gmail.com>
2015-07-12 18:41:37 +0300

    geehrc configs

---
+++ products/pa_geehrc.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2013 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+
+ifeq (pa_geehrc,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_geehrc
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Inherit telephony common stuff
+$(call inherit-product, vendor/pa/configs/telephony.mk)
+
+# Include AOSPA common configuration
+include vendor/pa/main.mk
+
+# Inherit device configuration
+$(call inherit-product, device/lge/geehrc/full_geehrc.mk)
+
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := geehrc
+PRODUCT_NAME := pa_geehrc
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := Optimus G
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=geehrc_open_eu \
+	BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.4.2/KOT49I.E97520a/E97520a.1403273885:user/release-keys \
+	PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.4.2 KOT49I.E97520a E97520a.1403273885 release-keys"
+
+endif
